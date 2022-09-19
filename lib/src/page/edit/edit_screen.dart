1e6af6cee09a7b4221484be8c8fc0080f8f5787f
atahatah <ratdotl.hata@gmail.com>
2022-09-19 22:57:52 +0900

    refactor: editable zoneを切り出し

--- lib/src/page/edit/edit_screen.dart
+++ lib/src/page/edit/edit_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_state_notifier/flutter_state_notifier.dart';
 import 'package:pixel_maker/src/controller/edit_screen/edit_screen_controller.dart';
@@ -7,6 +5,7 @@ import 'package:pixel_maker/src/controller/edit_screen/edit_screen_state.dart';
 import 'package:pixel_maker/src/controller/image/image_controller.dart';
 import 'package:pixel_maker/src/controller/image/image_state.dart';
 import 'package:pixel_maker/src/enums/edit_screen_enum.dart';
+import 'package:pixel_maker/src/page/edit/widet/editable_zone.dart';
 import 'package:provider/provider.dart';
 
 class EditScreen extends StatelessWidget {
@@ -24,83 +23,10 @@ class EditScreen extends StatelessWidget {
       child: Builder(builder: (context) {
         final screenState = context.watch<EditScreenState>();
         final screenController = context.read<EditScreenController>();
-        final imageController = context.read<ImageController>();
         return Scaffold(
-            body: Center(
-                child: SizedBox(
-                    height: 250,
-                    width: 250,
-                    child: LayoutBuilder(
-                      builder: (context, constraint) {
-                        final pixelBorderWidth =
-                            screenState.page == EditScreenEnum.preview
-                                ? 0.0
-                                : 1.0;
-                        final pixelOneSideSize =
-                            context.watch<ImageState>().size;
-                        final wholeOneSideLength =
-                            min(constraint.maxHeight, constraint.maxWidth) -
-                                pixelBorderWidth * 2;
-                        final pixelOneSideLength =
-                            wholeOneSideLength / pixelOneSideSize;
-                        return Center(
-                          child: InteractiveViewer(
-                            child: Container(
-                              decoration: BoxDecoration(
-                                  border:
-                                      screenState.page == EditScreenEnum.preview
-                                          ? null
-                                          : Border.all(
-                                              color: Colors.grey,
-                                              width: pixelBorderWidth)),
-                              child: Column(
-                                  children: context
-                                      .read<ImageState>()
-                                      .pixels
-                                      .map(
-                                        (rowPixels) => Row(
-                                          children: rowPixels
-                                              .map((e) => GestureDetector(
-                                                    onTap: () {
-                                                      if (screenState.page ==
-                                                          EditScreenEnum
-                                                              .preview) return;
-                                                      imageController
-                                                          .updateColor(
-                                                              e, Colors.red);
-                                                    },
-                                                    child: Container(
-                                                      padding: EdgeInsets.zero,
-                                                      width: pixelOneSideLength,
-                                                      height:
-                                                          pixelOneSideLength,
-                                                      decoration: BoxDecoration(
-                                                          border: screenState
-                                                                      .page ==
-                                                                  EditScreenEnum
-                                                                      .preview
-                                                              ? null
-                                                              : Border.all(
-                                                                  width:
-                                                                      pixelBorderWidth,
-                                                                  color: Colors
-                                                                      .grey,
-                                                                  strokeAlign:
-                                                                      StrokeAlign
-                                                                          .outside,
-                                                                ),
-                                                          color: e.color),
-                                                    ),
-                                                  ))
-                                              .toList(),
-                                        ),
-                                      )
-                                      .toList()),
-                            ),
-                          ),
-                        );
-                      },
-                    ))),
+            body: const Center(
+                child:
+                    SizedBox(height: 250, width: 250, child: EditableZone())),
             bottomNavigationBar: BottomNavigationBar(
               currentIndex: screenState.page.index,
               onTap: (value) =>
---
+++ lib/src/page/edit/widet/editable_zone.dart
@@ -0,0 +1,74 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:pixel_maker/src/controller/edit_screen/edit_screen_state.dart';
+import 'package:pixel_maker/src/controller/image/image_controller.dart';
+import 'package:pixel_maker/src/controller/image/image_state.dart';
+import 'package:pixel_maker/src/enums/edit_screen_enum.dart';
+import 'package:provider/provider.dart';
+
+class EditableZone extends StatelessWidget {
+  const EditableZone({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final screenState = context.watch<EditScreenState>();
+    final imageController = context.read<ImageController>();
+    final imageState = context.watch<ImageState>();
+    return LayoutBuilder(
+      builder: (context, constraint) {
+        final pixelBorderWidth =
+            screenState.page == EditScreenEnum.preview ? 0.0 : 1.0;
+        final pixelOneSideSize = imageState.size;
+        final wholeOneSideLength =
+            min(constraint.maxHeight, constraint.maxWidth) -
+                pixelBorderWidth * 2;
+        final pixelOneSideLength = wholeOneSideLength / pixelOneSideSize;
+        return Center(
+          child: InteractiveViewer(
+            child: Container(
+              decoration: BoxDecoration(
+                  border: screenState.page == EditScreenEnum.preview
+                      ? null
+                      : Border.all(
+                          color: Colors.grey, width: pixelBorderWidth)),
+              child: Column(
+                  children: imageState.pixels
+                      .map(
+                        (rowPixels) => Row(
+                          children: rowPixels
+                              .map((e) => GestureDetector(
+                                    onTap: () {
+                                      if (screenState.page ==
+                                          EditScreenEnum.preview) return;
+                                      imageController.updateColor(
+                                          e, Colors.red);
+                                    },
+                                    child: Container(
+                                      padding: EdgeInsets.zero,
+                                      width: pixelOneSideLength,
+                                      height: pixelOneSideLength,
+                                      decoration: BoxDecoration(
+                                          border: screenState.page ==
+                                                  EditScreenEnum.preview
+                                              ? null
+                                              : Border.all(
+                                                  width: pixelBorderWidth,
+                                                  color: Colors.grey,
+                                                  strokeAlign:
+                                                      StrokeAlign.outside,
+                                                ),
+                                          color: e.color),
+                                    ),
+                                  ))
+                              .toList(),
+                        ),
+                      )
+                      .toList()),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
