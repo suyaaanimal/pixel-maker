e084f3d1d464611867d1e59a626edd0a942f8568
atahatah <ratdotl.hata@gmail.com>
2022-09-20 14:33:41 +0900

    feat: scale bar

--- lib/src/page/edit/widet/editable_zone.dart
+++ lib/src/page/edit/widet/editable_zone.dart
@@ -1,4 +1,4 @@
-import 'dart:math';
+import 'dart:math' as math;
 
 import 'package:flutter/material.dart';
 import 'package:pixel_maker/src/controller/edit_screen/edit_screen_controller.dart';
@@ -16,7 +16,19 @@ class EditableZone extends StatefulWidget {
   State<EditableZone> createState() => _EditableZoneState();
 }
 
-class _EditableZoneState extends State<EditableZone> {
+class _EditableZoneState extends State<EditableZone>
+    with SingleTickerProviderStateMixin {
+  static const scaleMax = 4.0;
+  static const scaleMin = 1.0;
+
+  final transformationController = TransformationController();
+
+  @override
+  void dispose() {
+    transformationController.dispose();
+    super.dispose();
+  }
+
   void actionOnPixel(PixelModel e) {
     final screenState = context.read<EditScreenState>();
     final screenController = context.read<EditScreenController>();
@@ -48,47 +60,102 @@ class _EditableZoneState extends State<EditableZone> {
             screenState.page == EditScreenEnum.preview ? 0.0 : 1.0;
         final pixelOneSideSize = imageState.size;
         final wholeOneSideLength =
-            min(constraint.maxHeight, constraint.maxWidth) -
+            math.min(constraint.maxHeight, constraint.maxWidth) -
                 pixelBorderWidth * 2;
         final pixelOneSideLength = wholeOneSideLength / pixelOneSideSize;
         return Center(
-          child: InteractiveViewer(
-            panEnabled: false,
-            child: Container(
-              decoration: BoxDecoration(
-                  border: screenState.page == EditScreenEnum.preview
-                      ? null
-                      : Border.all(
-                          color: Colors.grey, width: pixelBorderWidth)),
-              child: Column(
-                  children: imageState.pixels
-                      .map(
-                        (rowPixels) => Row(
-                          children: rowPixels
-                              .map((e) => GestureDetector(
-                                    onTap: () => actionOnPixel(e),
-                                    child: Container(
-                                      padding: EdgeInsets.zero,
-                                      width: pixelOneSideLength,
-                                      height: pixelOneSideLength,
-                                      decoration: BoxDecoration(
-                                          border: screenState.page ==
-                                                  EditScreenEnum.preview
-                                              ? null
-                                              : Border.all(
-                                                  width: pixelBorderWidth,
-                                                  color: Colors.grey,
-                                                  strokeAlign:
-                                                      StrokeAlign.outside,
-                                                ),
-                                          color: e.color),
-                                    ),
-                                  ))
-                              .toList(),
-                        ),
-                      )
-                      .toList()),
-            ),
+          child: Column(
+            children: [
+              InteractiveViewer(
+                maxScale: scaleMax,
+                minScale: scaleMin,
+                transformationController: transformationController,
+                child: Container(
+                  decoration: BoxDecoration(
+                      border: screenState.page == EditScreenEnum.preview
+                          ? null
+                          : Border.all(
+                              color: Colors.grey, width: pixelBorderWidth)),
+                  child: Column(
+                      children: imageState.pixels
+                          .map(
+                            (rowPixels) => Row(
+                              children: rowPixels
+                                  .map((e) => GestureDetector(
+                                        onTap: () => actionOnPixel(e),
+                                        child: Container(
+                                          padding: EdgeInsets.zero,
+                                          width: pixelOneSideLength,
+                                          height: pixelOneSideLength,
+                                          decoration: BoxDecoration(
+                                              border: screenState.page ==
+                                                      EditScreenEnum.preview
+                                                  ? null
+                                                  : Border.all(
+                                                      width: pixelBorderWidth,
+                                                      color: Colors.grey,
+                                                      strokeAlign:
+                                                          StrokeAlign.outside,
+                                                    ),
+                                              color: e.color),
+                                        ),
+                                      ))
+                                  .toList(),
+                            ),
+                          )
+                          .toList()),
+                ),
+              ),
+              AnimatedBuilder(
+                  animation: transformationController,
+                  builder: (context, child) {
+                    return Slider(
+                        min: scaleMin,
+                        max: scaleMax,
+                        value:
+                            transformationController.value.getMaxScaleOnAxis(),
+                        onChanged: (value) {
+                          final deltaScale = value /
+                              transformationController.value
+                                  .getMaxScaleOnAxis();
+                          transformationController.value.scale(deltaScale);
+
+                          final deltaTrns = (deltaScale - 1.0) / 2.0;
+                          double transX = -deltaTrns * wholeOneSideLength;
+                          double transY = -deltaTrns * wholeOneSideLength;
+                          transformationController.value
+                              .translate(transX, transY);
+
+                          final currentTrns =
+                              transformationController.value.getTranslation();
+                          final currentScale = transformationController.value
+                              .getMaxScaleOnAxis();
+
+                          double fixTransX = 0.0;
+                          double fixTransY = 0.0;
+                          if (currentTrns.x > 0) {
+                            fixTransX = currentTrns.x;
+                          } else if (-(currentTrns.x - wholeOneSideLength) >
+                              wholeOneSideLength * currentScale) {
+                            fixTransX = currentTrns.x -
+                                wholeOneSideLength +
+                                wholeOneSideLength * currentScale;
+                          }
+                          if (currentTrns.y > 0) {
+                            fixTransY = currentTrns.y;
+                          } else if (-(currentTrns.y - wholeOneSideLength) >
+                              wholeOneSideLength * currentScale) {
+                            fixTransY = currentTrns.y -
+                                wholeOneSideLength +
+                                wholeOneSideLength * currentScale;
+                          }
+                          transformationController.value
+                              .translate(-fixTransX, -fixTransY);
+                          setState(() {});
+                          // Transform();
+                        });
+                  })
+            ],
           ),
         );
       },
--- lib/src/route/route.dart
+++ lib/src/route/route.dart
@@ -2,15 +2,18 @@ import 'package:go_router/go_router.dart';
 import 'package:pixel_maker/src/page/edit/edit_screen.dart';
 import 'package:pixel_maker/src/page/home_screen.dart';
 
-final route = GoRouter(routes: [
-  GoRoute(
-      path: '/',
-      builder: ((context, state) => HomeScreen(
-            key: state.pageKey,
-          ))),
-  GoRoute(
-      path: '/edit',
-      builder: ((context, state) => EditScreen(
-            key: state.pageKey,
-          )))
-]);
+final route = GoRouter(
+  initialLocation: '/edit',
+  routes: [
+    GoRoute(
+        path: '/',
+        builder: ((context, state) => HomeScreen(
+              key: state.pageKey,
+            ))),
+    GoRoute(
+        path: '/edit',
+        builder: ((context, state) => EditScreen(
+              key: state.pageKey,
+            )))
+  ],
+);
